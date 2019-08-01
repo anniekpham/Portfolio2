2a3d1cb33539b7d29698398185c79bb70814a5e4
Annie Pham <anniekpham@yahoo.com>
2019-08-01 09:38:18 -0700

    fix error

--- client/src/pages/about/about.js
+++ client/src/pages/about/about.js
@@ -30,7 +30,7 @@ const About = _ => {
                 <h2 className="subtitle">Hello World,</h2>
                 <div className="aboutcontainer">
                     <img src={Picture} className="picture" />
-                    <p className="abouttext">and welcome to my portfolio! My name is Annie and I am a Full-Stack Web Developer located in Monclair, California. I graduated from Cal State Fullerton with a degree in Business Management. However, ever since learning how to code I've fell in love with programing and never look back. I am passionate about learning new languages and developing cool new applications that are fun and useful, so I would love to collaborate on new projects!</p>
+                    <p className="abouttext">and welcome to my portfolio! My name is Annie and I am a Full-Stack Web Developer located in Montclair, California. I graduated from Cal State Fullerton with a degree in Business Management. However, ever since learning how to code I've fell in love with programming and never look back. I am passionate about learning new languages and developing cool new applications that are fun and useful, so I would love to collaborate on new projects!</p>
                     <div className={classes.root} id="maincontainer">
                         <Grid container spacing={3}>
                             <Grid item lg={4} md={4} xs={12} className="gridcontainer">
